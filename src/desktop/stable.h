d1e859abbac4ff71f5648904f942708d92b01202
Jake Petroules <jake.petroules@petroules.com>
2012-08-14 00:53:35 -0400

    Remove qmake project files - we're converting to CMake.
    Also remove precompiled headers, they're not all that necessary.

--- src/desktop/stable.h
+++
@@ -1,9 +0,0 @@
-#ifndef STABLE_H
-#define STABLE_H
-
-#if defined __cplusplus
-#include <QtGui>
-#include <QtNetwork>
-#endif
-
-#endif // STABLE_H
--- src/libqt/stable.h
+++
@@ -1,9 +0,0 @@
-#ifndef STABLE_H
-#define STABLE_H
-
-#if defined __cplusplus
-#include <QtXml>
-#include <petroules-utilities.h>
-#endif
-
-#endif // STABLE_H
