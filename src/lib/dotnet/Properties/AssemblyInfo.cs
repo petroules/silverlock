443d528af24b6b34404c31ac745f4cd846b7106b
Jake Petroules <jake.petroules@petroules.com>
2012-08-19 21:41:05 -0400

    Unified .NET and Silverlight C# projects into a Portable Library project.

--- src/lib/dotnet/Properties/AssemblyInfo.cs
+++ src/lib/dotnet/Properties/AssemblyInfo.cs
@@ -1,27 +1,19 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Silverlock Library for Silverlight")]
+[assembly: AssemblyTitle("Silverlock Library")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Petroules Corporation")]
 [assembly: AssemblyProduct("Silverlock")]
-[assembly: AssemblyCopyright("Copyright © 2010 Petroules Corporation")]
+[assembly: AssemblyCopyright("Copyright © 2012 Petroules Corporation")]
 [assembly: AssemblyTrademark("All Rights Reserved")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("db95c6d1-559c-443c-ae83-fb84775b9add")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -30,8 +22,9 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
