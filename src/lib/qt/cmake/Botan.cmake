e38d9c6228d63528559c35c5fca7342e202e2371
Jake Petroules <jake.petroules@petroules.com>
2012-08-20 09:33:46 -0400

    Finished universal binary implementation for Botan and allowed Botan's configure to infer more things on its own.

--- src/lib/qt/cmake/Botan.cmake
+++ src/lib/qt/cmake/Botan.cmake
@@ -2,52 +2,22 @@ if(NOT "${CMAKE_BUILD_TYPE}" STREQUAL "Release")
     set(BOTAN_DEBUG "--enable-debug")
 endif()
 
+include(BotanUniversal)
+
 if(CMAKE_TARGET_ARCHITECTURE_UNIVERSAL)
-    set(BOTAN_ARCH_FILE "${CMAKE_SOURCE_DIR}/lib/botan/src/build-data/arch/universal.txt")
-    set(BOTAN_CC_FILES
-        "${CMAKE_SOURCE_DIR}/lib/botan/src/build-data/cc/gcc.txt"
-        "${CMAKE_SOURCE_DIR}/lib/botan/src/build-data/cc/clang.txt"
-    )
-
-    # create empty file
-    file(WRITE "${BOTAN_ARCH_FILE}" "")
-
-    set(BOTAN_CC_DATA "universalbinary -> \"-force_cpusubtype_ALL -mmacosx-version-min=10.4")
-    foreach(osx_arch ${CMAKE_TARGET_ARCHITECTURES})
-        set(BOTAN_CC_DATA "${BOTAN_CC_DATA} -arch ${osx_arch}")
-    endforeach()
-
-    # Closing quote
-    set(BOTAN_CC_DATA "${BOTAN_CC_DATA}\"")
-
-    # Edit the compiler files
-    foreach(f ${BOTAN_CC_FILES})
-        file(READ "${f}" contents)
-        string(REPLACE "</mach_abi_linking>" "${BOTAN_CC_DATA}\n</mach_abi_linking>" contents "${contents}")
-        file(WRITE "${f}" "${contents}")
-    endforeach()
+    botan_prepare_universal()
+    set(BOTAN_CPU "--cpu=universal")
 else()
-    if("${CMAKE_TARGET_ARCHITECTURES}" STREQUAL "i386")
-        set(BOTAN_CPU "--cpu=i686") # Botan likes i686 for Intel 32-bit instead of i386
-    else()
-        set(BOTAN_CPU "--cpu=${CMAKE_TARGET_ARCHITECTURES}")
-    endif()
+    set(BOTAN_CPU "--cpu=${CMAKE_TARGET_ARCHITECTURES}")
 endif()
 
 if(ANDROID)
     set(BOTAN_OS "--os=android")
     set(BOTAN_CPU "--cpu=arm")
     set(BOTAN_OPTS "--no-autoload --enable-modules=auto_rng,pbkdf2,dev_random,sha1,aes,cbc,pkcs7")
-elseif(WIN32)
-    if(MSVC)
-        set(BOTAN_OS "--os=windows")
-    else()
-        set(BOTAN_OS "--os=mingw")
-    endif()
-elseif(APPLE)
-    set(BOTAN_OS "--os=macosx")
-else()
-    # Linux and other OSes can probably handle themselves...
+elseif(WIN32 AND NOT MSVC)
+    # Botan tends to detect OS as "Windows" even when not in MSVC shell
+    set(BOTAN_OS "--os=mingw")
 endif()
 
 # Don't quote this
@@ -92,6 +62,6 @@ if(botan_reconfigure)
     endif()
 endif()
 
-if(CMAKE_TARGET_ARCHITECTURE_UNIVERSAL AND EXISTS "${BOTAN_ARCH_FILE}")
-    file(REMOVE "${BOTAN_ARCH_FILE}")
+if(CMAKE_TARGET_ARCHITECTURE_UNIVERSAL)
+    botan_revert_universal()
 endif()
