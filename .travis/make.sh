fb8e9751f1ffbb33bae94af3c13205b17a2f58be
Jake Petroules <jake.petroules@petroules.com>
2012-09-26 05:29:00 -0400

    Build all targets in Travis.

--- .travis/make.sh
+++ .travis/make.sh
@@ -3,5 +3,5 @@ set -e
 
 # Build all variants
 cd ..
-make -C build-shared package
-make -C build-static package
+make -C build-shared docs package package_source
+make -C build-static docs package package_source
