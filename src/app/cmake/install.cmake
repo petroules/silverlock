cd34c4bbb832efc4af4822c1ed82c90d4d91b659
Jake Petroules <jake.petroules@petroules.com>
2012-10-05 20:31:03 -0400

    Simplify bundle fixup.

--- src/app/cmake/install.cmake
+++ src/app/cmake/install.cmake
@@ -4,20 +4,8 @@ if(IS_RELEASE_BUILD)
         RUNTIME DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_REL} COMPONENT Runtime
     )
 
-    if(WIN32 OR APPLE)
-        if(WIN32)
-            set(ESUFFIX ${CMAKE_EXECUTABLE_SUFFIX})
-        elseif(APPLE)
-            set(ESUFFIX .app)
-        endif()
-
-        list(APPEND LIBDIRS "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
-        list(APPEND LIBDIRS "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${CMAKE_BUILD_TYPE}")
-        list(APPEND LIBDIRS "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}")
-        list(APPEND LIBDIRS "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/${CMAKE_BUILD_TYPE}")
-        list(APPEND PLUGINS qtaccessiblewidgets qcncodecs qjpcodecs qkrcodecs qtwcodecs qsvgicon qgif qico qjpeg qmng qsvg qtiff)
-        
+    if(WIN32 OR APPLE)        
         include(DeployQt4)
-        install_qt4_executable("${TARGET_NAME}${ESUFFIX}" "${PLUGINS}" "" "${LIBDIRS}" "" "" Runtime)
+        install_qt4_executable("${TARGET_NAME}${CMAKE_USER_EXECUTABLE_SUFFIX}" "qtaccessiblewidgets qcncodecs qjpcodecs qkrcodecs qtwcodecs qsvgicon qgif qico qjpeg qmng qsvg qtiff" "" "${ALL_LIBRARY_DIRS}" "" "" Runtime)
     endif()
 endif()
