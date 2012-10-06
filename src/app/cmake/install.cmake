9a26b8103ea32bd1785b3783a53c86a483f796e4
Jake Petroules <jake.petroules@petroules.com>
2012-10-05 20:45:35 -0400

    Move CMake includes to main file.

--- src/app/cmake/install.cmake
+++ src/app/cmake/install.cmake
@@ -4,8 +4,7 @@ if(IS_RELEASE_BUILD)
         RUNTIME DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_REL} COMPONENT Runtime
     )
 
-    if(WIN32 OR APPLE)        
-        include(DeployQt4)
+    if(WIN32 OR APPLE)
         install_qt4_executable("${TARGET_NAME}${CMAKE_USER_EXECUTABLE_SUFFIX}" "qtaccessiblewidgets qcncodecs qjpcodecs qkrcodecs qtwcodecs qsvgicon qgif qico qjpeg qmng qsvg qtiff" "" "${ALL_LIBRARY_DIRS}" "" "" Runtime)
     endif()
 endif()
